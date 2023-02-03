bc91e26277d303b720d65e617a38c744e3771cfe
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-02-04 00:05:51 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './components/Header'
 import Home from './components/Home'
 import Login from './components/Login'
+import Details from './components/Details'
+import Error from './components/Error'
 import {Routes, Route} from 'react-router-dom'
 
 function App() {
